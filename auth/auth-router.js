69914381c2f0d40fd9fde43d6ab84eb48a5da744
ebisLab <ebaiden@techie.com>
2019-09-20 09:23:26 -0400

    register endpoint implemented

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,9 +1,21 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 const Jokes = require('../jokes/jokes-model');
 
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+  user.password = hash;
+
+  Jokes.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 router.post('/login', (req, res) => {
