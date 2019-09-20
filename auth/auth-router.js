226a73cc95f44be08e2bbe903a4b13137285c13c
ebisLab <ebaiden@techie.com>
2019-09-20 10:44:42 -0400

    login endpoint implemented

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -20,6 +20,34 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
   // implement login
+  let { username, password } = req.body;
+
+  Jokes.findBy({ username })
+    .first()
+    .then(user => {
+      //check
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
+
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+  }
+
+  const options = {
+    //expiration of the token
+    expiresIn: '1d', //one day
+  }
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
+
 
 module.exports = router;
---
+++ auth/secrets.js
@@ -0,0 +1,3 @@
+module.exports = {
+    jwtSecret: process.env.JWT_SECRET || 'keept it secrt, keep it safe! '
+}
